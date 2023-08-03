0b74887d7d6bc16638fd841e1f908a7fe31dc463
ilia <kavaleuilia@gmail.com>
2023-08-03 20:26:27 +0200

    Add models Cart and CartItem

--- ecommerce_project/shop/urls.py
+++ ecommerce_project/shop/urls.py
@@ -5,6 +5,8 @@ app_name = 'shop'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('<slug:category_slug>', views.home, name='products_by_category'),
-    path('product/', views.product, name='product')
+    path('category/<slug:category_slug>', views.home, name='products_by_category'),
+    path('category/<slug:category_slug>/<slug:product_slug>', views.product, name='product_detail'),
+    path('product/', views.product, name='product'),
+    path('cart/', views.cart, name='cart')
 ]
--- ecommerce_project/shop/views.py
+++ ecommerce_project/shop/views.py
@@ -16,5 +16,14 @@ def home(request, category_slug=None):
     return render(request, 'shop/home.html', context)
 
 
-def product(request):
-    return render(request, 'shop/product.html')
+def product(request, category_slug, product_slug):
+    try:
+        product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e
+    context = {'product': product}
+    return render(request, 'shop/product.html', context)
+
+
+def cart(request):
+    return render(request, 'shop/cart.html')
